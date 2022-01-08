debc7e8f017e83b34467498fd6066c329fd57bd2
ChenJialong <2833275565@qq.com>
2022-01-08 20:35:36 +0800

    some trim.

--- src/main/java/HttpUtil.java
+++ src/main/java/HttpUtil.java
@@ -4,15 +4,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.zip.GZIPInputStream;
-
 public class HttpUtil {
     public static StringBuilder getCityJSON(String key, String cityName) throws IOException {
         String cityQueryUrl = "https://geoapi.qweather.com/v2/city/lookup?key="
